49f5f9c1b8f7c9d557017c83ddb3a0f8cd00c2a6
Mohammad Mousavifard <91153622+mmousavifard@users.noreply.github.com>
2023-03-25 12:30:41 +0330

    Update post_relax.sh

--- scripts/post_relax.sh
+++ scripts/post_relax.sh
@@ -13,9 +13,13 @@ gmx grompp -f ../data/martini_eqxl.mdp -c ../min/min$(($1+1)).gro -p ../topol.to
 gmx mdrun -v -deffnm ../md/md$(($1+1)) -nt $2
 if [ -f "../md/md$(($1+1)).gro" ]; then
     echo 0 | gmx trjconv -f ../md/md$(($1+1)).xtc -s ../md/md$(($1+1)).tpr -o ../md/md$(($1+1)).gro -b $3 -e $3
-else 
+else
+    gmx grompp -f ../data/martini_em.mdp -c ../md/temp.gro -p ../topol.top -o ../min/min$(($1+1)).tpr -maxwarn 1
+    gmx mdrun -v -deffnm ../min/min$(($1+1)) -nt $2
+    gmx grompp -f ../data/martini_eqxl.mdp -c ../min/min$(($1+1)).gro -p ../topol.top -o ../md/md$(($1+1)).tpr
     gmx mdrun -v -deffnm ../md/md$(($1+1)) -nt $2
-	echo 0 | gmx trjconv -f ../md/md$(($1+1)).xtc -s ../md/md$(($1+1)).tpr -o ../md/md$(($1+1)).gro -b $3 -e $3
+    echo 0 | gmx trjconv -f ../md/md$(($1+1)).xtc -s ../md/md$(($1+1)).tpr -o ../md/md$(($1+1)).gro -b $3 -e $3
 fi
 rm ../md/#md$(($1+1)).gro.1#
+rm ../min/#min$(($1+1)).gro.1#
 rm ../md/temp.gro
