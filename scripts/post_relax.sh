bd41e93227ea5a153ddf08116a8804fd70be22ca
Mohammad Mousavifard <91153622+mmousavifard@users.noreply.github.com>
2023-09-29 17:16:03 +0330

    Update post_relax.sh

--- scripts/post_relax.sh
+++ scripts/post_relax.sh
@@ -7,10 +7,10 @@ cp ../product.itp ../itp/loop$(($1+1)).itp
 if [ -f "../md/md$(($1+1)).gro" ]; then
     rm ../md/md$(($1+1)).gro
 fi
-gmx grompp -f ../data/martini_em.mdp -c ../md/temp.gro -p ../topol.top -o ../min/min$(($1+1)).tpr -maxwarn 1
-gmx mdrun -v -deffnm ../min/min$(($1+1)) -nt $2
-gmx grompp -f ../data/martini_eqxl.mdp -c ../min/min$(($1+1)).gro -p ../topol.top -o ../md/md$(($1+1)).tpr
-gmx mdrun -v -deffnm ../md/md$(($1+1)) -nt $2
+#gmx grompp -f ../data/martini_em.mdp -c ../md/temp.gro -p ../topol.top -o ../min/min$(($1+1)).tpr -maxwarn 1
+#gmx mdrun -v -deffnm ../min/min$(($1+1)) -nt $2
+#gmx grompp -f ../data/martini_eqxl.mdp -c ../min/min$(($1+1)).gro -p ../topol.top -o ../md/md$(($1+1)).tpr
+#gmx mdrun -v -deffnm ../md/md$(($1+1)) -nt $2
 if [ -f "../md/md$(($1+1)).gro" ]; then
     echo 0 | gmx trjconv -f ../md/md$(($1+1)).xtc -s ../md/md$(($1+1)).tpr -o ../md/md$(($1+1)).gro -b $3 -e $3
 else
