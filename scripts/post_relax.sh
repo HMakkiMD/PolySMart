4ace84e4ba772c4aa81086385dbe20ec2351faee
Mohammad Mousavifard <s.m.mousavifard@gmail.com>
2024-01-29 18:23:46 +0330

    Update post_relax.sh

--- scripts/post_relax.sh
+++ scripts/post_relax.sh
@@ -7,16 +7,16 @@ cp ../product.itp ../itp/loop$(($1+1)).itp
 if [ -f "../md/md$(($1+1)).gro" ]; then
     rm ../md/md$(($1+1)).gro
 fi
-#gmx grompp -f ../data/martini_em.mdp -c ../md/temp.gro -p ../topol.top -o ../min/min$(($1+1)).tpr -maxwarn 1
-#gmx mdrun -v -deffnm ../min/min$(($1+1)) -nt $2
-#gmx grompp -f ../data/martini_eqxl.mdp -c ../min/min$(($1+1)).gro -p ../topol.top -o ../md/md$(($1+1)).tpr
-#gmx mdrun -v -deffnm ../md/md$(($1+1)) -nt $2
+gmx grompp -f ../data/martini_em.mdp -c ../md/temp.gro -p ../topol.top -o ../min/min$(($1+1)).tpr -maxwarn 1
+gmx mdrun -v -deffnm ../min/min$(($1+1)) -nt $2
+gmx grompp -f ../data/martini_eqxl.mdp -c ../min/min$(($1+1)).gro -p ../topol.top -o ../md/md$(($1+1)).tpr
+gmx mdrun -v -deffnm ../md/md$(($1+1)) -nt $2
 if [ -f "../md/md$(($1+1)).gro" ]; then
     echo 0 | gmx trjconv -f ../md/md$(($1+1)).xtc -s ../md/md$(($1+1)).tpr -o ../md/md$(($1+1)).gro -dump $3
 else
-    gmx grompp -f ../data/martini_em.mdp -c ../md/temp.gro -p ../topol.top -o ../min/min$(($1+1)).tpr -maxwarn 1
-    gmx mdrun -v -deffnm ../min/min$(($1+1)) -nt $2
-    gmx grompp -f ../data/martini_eqxl.mdp -c ../min/min$(($1+1)).gro -p ../topol.top -o ../md/md$(($1+1)).tpr
+    gmx grompp -f ../data/martini_eqxl1.mdp -c ../min/min$(($1+1)).gro -p ../topol.top -o ../md/eq$(($1+1)).tpr -maxwarn 1
+    gmx mdrun -v -deffnm ../md/eq$(($1+1)) -nt $2
+    gmx grompp -f ../data/martini_eqxl.mdp -c ../md/eq$(($1+1)).gro -p ../topol.top -o ../md/md$(($1+1)).tpr
     gmx mdrun -v -deffnm ../md/md$(($1+1)) -nt $2
     echo 0 | gmx trjconv -f ../md/md$(($1+1)).xtc -s ../md/md$(($1+1)).tpr -o ../md/md$(($1+1)).gro -dump $3
 fi
